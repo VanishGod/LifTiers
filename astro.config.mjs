c1a8141d6307cb7414275d4ea8149bd0f32f1989
VanishGod <lianperezchacon@ejemplo.com>
2026-08-09 13:07:24 +0200

    astro config corregido

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],  
+    plugins: [tailwindcss()],
   },
-
+  server: {
+    port: 4321,
+    host: true,
+  },
   integrations: [react()],
 });
